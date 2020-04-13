2b8d096b04979cbd2f8b7049271a18a62468f117
Will Liao <will.liao@Nextlink.com.tw>
2020-04-13 14:52:20 +0800

    Add DynamoDB service

---
+++ services/docClient.js
@@ -0,0 +1,9 @@
+const AWS = require('aws-sdk')
+AWS.config.update({
+  region: process.env.AWS_DYNAMODB_REGION
+})
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+module.exports = docClient
+
+// use by seeder
