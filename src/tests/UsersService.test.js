2fdddf84ffd9fc3563e3987632914dfee9f6af39
Will Liao <will.liao@hp.com>
2022-06-07 16:31:41 +0800

    update ut

--- src/tests/UsersService.test.js
+++ src/tests/UsersService.test.js
@@ -1,33 +1,39 @@
 // const usersService = require('../services/UsersService')
-const usersModel = require('../models/users')
 const { expect } = require('expect')
+const usersModel = require('../models/users')
 
 const users = [
     {
         email: 'wyliao76@gmail.com',
     },
 ]
+const request = {
+    body: {
+        email: users[0].email,
+    },
+}
+// let's do TDD yo
+const usersPOST = async (request) => {
+    const { email } = request.body
+    if (!email) throw new Error('missing email address.')
+    const result = await usersModel.create({
+        email,
+    })
+    return result
+}
 
 describe('users service', () => {
-    it('should post', async () => {
-        // let's do TDD yo
-        const usersPOST = async () => {
-            const { email } = request.body
-            if (!email) throw new Error('missing email address.')
-            const result = await usersModel.create({
-                email,
-            })
-            return result
-        }
-        const request = {
-            body: {
-                email: users[0].email,
-            },
-        }
-        const result = await usersPOST(request)
-        expect(result.email).toBe(users[0].email)
-        // const { code, payload: file } = await usersService.usersPOST(request)
-        // expect(code).toBe(200)
-        // expect(file.email).toBe(users[0].email)
+    describe('users POST', () => {
+        it('should post', async () => {
+            const result = await usersPOST(request)
+            expect(result.email).toBe(users[0].email)
+            // const { code, payload: file } = await usersService.usersPOST(request)
+            // expect(code).toBe(200)
+            // expect(file.email).toBe(users[0].email)
+        })
+
+        it('should throw an error (bad request)', async () => {
+            await expect(usersPOST({ body: {} })).rejects.toThrow('missing email address.')
+        })
     })
 })
